0f765f675d33d23eaffbcd71cb1bf961db8b036f
yangmw7 <yangmw7@gmail.com>
2025-05-04 02:13:19 +0900

    feat: React 프로젝트 초기 커밋

--- src/App.js
+++ src/App.js
@@ -1,25 +1,21 @@
-import logo from './logo.svg';
-import './App.css';
+import { useEffect, useState } from "react";
 
 function App() {
+  const [message, setMessage] = useState("");
+
+  useEffect(() => {
+    fetch("http://localhost:8080/api/hello")
+      .then(res => res.text())
+      .then(data => setMessage(data));
+  }, []);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h1>백엔드에서 온 메시지:</h1>
+      <p>{message}</p>
     </div>
   );
 }
 
 export default App;
+
